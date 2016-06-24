5c293dba6613a59535e76cbe87b40790edfc3f0c
liuderchi <liuderchi@gmail.com>
2016-06-24 10:04:58 +0800

    [init] init by atom pkg generator
    - pkg name: vtt-helper

---
+++ spec/vtt-helper-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import VttHelperView from '../lib/vtt-helper-view';
+
+describe('VttHelperView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
