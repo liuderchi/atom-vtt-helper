1b5cb1d4017a59c02e84ac9482ab896c05af3c58
liuderchi <liuderchi@gmail.com>
2016-06-28 10:44:33 +0800

    [feat] REMOVE vtt-helper:toggle cmd
    - remove spec files (x2)
    - remove vtt-helper-view.js
    - update menu and keymap
    - remove dependency in vtt-helper.js
    - update package.json

--- spec/vtt-helper-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import VttHelperView from '../lib/vtt-helper-view';
-
-describe('VttHelperView', () => {
-  it('has one valid test', () => {
-    expect('easy').toBe('easy');
-  });
-});
